4219059156dccbfa9ceb048aa061ccd3440ef69a
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2023-12-29 18:02:11 +0100

    playing with zustand

--- src/app/components/LocationOffers/index.tsx
+++ src/app/components/LocationOffers/index.tsx
@@ -1,6 +1,21 @@
-import styles from "./LocationOffers.module.scss";
-// import NavBar from "@/app/components/NavBar";
+// 'use client';
+
+import StoreInitializer from '@/app/storeInitializer';
+import styles from './LocationOffers.module.scss';
+import useStore from '@/app/store';
+import { SetMode } from './components/SetMode';
+import { Button } from './components/Button';
 
 export default function LocationOffers() {
-	return <main className={styles.containerWrap}>{/* <NavBar /> */}</main>;
+    // const randomNum = (Math.random() * 1000).toFixed(0);
+    // console.log(randomNum);
+    const isDark = useStore.getState().darkMode;
+    return (
+        <main className={styles.containerWrap}>
+            <StoreInitializer darkMode={isDark} />
+            <div className={styles.item}>{'AAa'}</div>
+            <SetMode />
+            <Button />
+        </main>
+    );
 }
---
+++ src/app/storeInitializer.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { useRef } from 'react';
+import useStore from './store';
+
+function StoreInitializer({ darkMode }: { darkMode: boolean }) {
+    const initialized = useRef(false);
+    if (!initialized.current) {
+        useStore.setState({ darkMode });
+        initialized.current = true;
+    }
+    return null;
+}
+export default StoreInitializer;
