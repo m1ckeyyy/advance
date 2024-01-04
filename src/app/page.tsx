0fefc5618a0d55b73d2827e7c7b1b638e159cd58
Mikołaj Krawczyński <mkrawczynski1@sii.pl>
2024-01-04 14:50:29 +0100

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 // import styles from "./page.module.css";
 // import LandingPage from "./components/LandingPage";
 // import LocationOffers from "./components/LocationOffers";
+//      t est comment
 import { LandingPage, LocationOffers } from './components';
 export default function Home() {
     return (
