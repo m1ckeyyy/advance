8fcf42ceccf27f11f4830f76be7f41c9701c2984
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2024-01-11 18:39:36 +0100

    styling

--- src/app/components/LocationOffers/index.tsx
+++ src/app/components/LocationOffers/index.tsx
@@ -1,21 +1,14 @@
-// 'use client';
-
 import StoreInitializer from '@/app/storeInitializer';
 import styles from './LocationOffers.module.scss';
 import useStore from '@/app/store';
-import { SetMode } from './components/SetMode';
-import { Button } from './components/Button';
+import CardOffer from './components/CardOffer';
 
 export default function LocationOffers() {
-    // const randomNum = (Math.random() * 1000).toFixed(0);
-    // console.log(randomNum);
-    const isDark = useStore.getState().darkMode;
     return (
         <main className={styles.containerWrap}>
-            <StoreInitializer darkMode={isDark} />
-            <div className={styles.item}>{'AAa'}</div>
-            <SetMode />
-            <Button />
+            <div>
+                <CardOffer />
+            </div>
         </main>
     );
 }
