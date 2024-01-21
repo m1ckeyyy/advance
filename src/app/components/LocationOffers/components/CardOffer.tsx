e37d999c3edf25b2842c796874796baddf964b9d
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2024-01-21 18:35:46 +0100

    added custom component as cardOffer

--- src/app/components/LocationOffers/components/CardOffer.tsx
+++ src/app/components/LocationOffers/components/CardOffer.tsx
@@ -4,17 +4,27 @@ import { DM_Sans } from 'next/font/google';
 
 const dmSans = DM_Sans({ subsets: ['latin'], weight: '300' });
 
-const CardOffer = () => {
+const CardOffer = ({
+    imgsrc,
+    name,
+    classReference,
+}: {
+    imgsrc: string;
+    name: string;
+    classReference: string;
+}) => {
     return (
-        <div className={`${styles.cardOffer} ${dmSans.className}`}>
+        <div
+            className={`${styles.cardOffer} ${dmSans.className} ${styles[classReference]} `}
+        >
             <Image
-                src="/krakow.jpg"
+                src={imgsrc}
                 width={512 * 1.15}
-                height={398 * 1.15}
-                alt="man"
-                className={styles.krakowImg}
+                height={398 * 1.21}
+                alt={name}
+                className={styles.image}
             />
-            <span>Kraków</span>
+            <span>{name}</span>
         </div>
     );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from "next";
-import { Inter, DM_Sans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter, DM_Sans } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
-const DMSans = DM_Sans({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
+const DMSans = DM_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: "AAA",
-	description: "Aaa",
+    title: 'AAA',
+    description: 'Aaa',
 };
 
 export default function RootLayout({
-	children,
+    children,
 }: {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
