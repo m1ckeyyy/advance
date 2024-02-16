97d465fd7146335d2b4b38a9fe1167179b3aa663
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2024-02-16 23:58:13 +0100

    footer

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,37 @@
+import styles from './Footer.module.scss';
+import { DM_Sans } from 'next/font/google';
+import { FaFacebook } from 'react-icons/fa';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+const dmSans = DM_Sans({ subsets: ['latin'], weight: '300' });
+
+export default function Footer() {
+    return (
+        <div className={`${styles.containerWrap} ${dmSans.className}`}>
+            <div className={styles.rightsReserved}>
+                <h4>© {new Date().getFullYear()} Advance | Wszelkie prawa zastrzeżone.</h4>
+            </div>
+            <div className={styles.contactRow}>
+                <span className={styles.rowHeader}>Kontakt</span>
+                <span>Advance Nieruchomości</span>
+                <span>ul. Walerego Sławka 16C/L2</span>
+                <span>+48 535 772 875</span>
+                <span>biuro@advance.nieruchomosci.pl</span>
+            </div>
+            <div className={styles.navigationRow}>
+                <span className={styles.rowHeader}>Nawiguj</span>
+                <span>Oferty</span>
+                <span>O nas</span>
+                <span>Opinie</span>
+                <span>Kontakt</span>
+            </div>
+            <div className={styles.facebook} title="Facebook">
+                <Link href={'https://www.facebook.com/profile.php?id=100057415211111'} target="_blank">
+                    <FaFacebook size="2rem" color="white" />
+                </Link>
+            </div>
+        </div>
+    );
+}
