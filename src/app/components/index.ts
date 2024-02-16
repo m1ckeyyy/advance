ed618955953c99ae0893e2559739f323266c1c58
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2024-02-16 23:58:13 +0100

    footer

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,4 +3,5 @@ import LandingPage from './LandingPage';
 import LocationOffers from './LocationOffers';
 import Testimonials from './Testimonials';
 import AboutUs from './AboutUs';
-export { LandingPage, LocationOffers, Testimonials, AboutUs };
+import Footer from './Footer';
+export { LandingPage, LocationOffers, Testimonials, AboutUs, Footer };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,15 @@ import styles from './page.module.css';
 // import LandingPage from "./components/LandingPage";
 // import LocationOffers from "./components/LocationOffers";
 //      t est comment
-import { LandingPage, LocationOffers, Testimonials, AboutUs } from './components';
+import { LandingPage, LocationOffers, Testimonials, AboutUs, Footer } from './components';
 export default function Home() {
     return (
-        <>
+        <div>
             <LandingPage />
             <LocationOffers />
             <AboutUs />
             <Testimonials />
-        </>
+            <Footer />
+        </div>
     );
 }
