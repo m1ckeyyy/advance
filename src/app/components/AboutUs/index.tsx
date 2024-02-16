fceee1ed565389074a17fda61411ee992bbfb713
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2024-02-16 12:27:57 +0100

    tweaks for mobile view2

--- src/app/components/AboutUs/index.tsx
+++ src/app/components/AboutUs/index.tsx
@@ -12,10 +12,11 @@ export default function AboutUs() {
             <h3 className={styles.description}>
                 <Image src="/logo3.png" alt="Small logo" width={40} height={40} />:
                 <span>
-                    Jesteśmy firmą specjalizującą się w pośrednictwie w obrocie nieruchomościami oraz ich wycenie. Pasjonujemy się nieruchomościami i podejmujemy się każdego
-                    klienta z indywidualnym podejściem. Nasza firma oferuje kompleksowe doradztwo w zakresie inwestowania w nieruchomości, a współpracujemy z wieloma specjalistami,
-                    aby efektywnie zarządzać rynkiem nieruchomości i oszczędzać czas oraz pieniądze naszych klientów. Posiadamy wszystkie niezbędne uprawnienia zawodowe, zgodne z
-                    obowiązującymi przepisami prawnymi.
+                    Jesteśmy firmą specjalizującą się w pośrednictwie w obrocie nieruchomościami oraz ich wycenie.
+                    <br />
+                    Pasjonujemy się nieruchomościami i podejmujemy się każdego klienta z indywidualnym podejściem. Nasza firma oferuje kompleksowe doradztwo w zakresie inwestowania
+                    w nieruchomości, a współpracujemy z wieloma specjalistami, aby efektywnie zarządzać rynkiem nieruchomości i oszczędzać czas oraz pieniądze naszych klientów.
+                    Posiadamy wszystkie niezbędne uprawnienia zawodowe, zgodne z obowiązującymi przepisami prawnymi.
                 </span>
             </h3>
             <div className={styles.descriptionFooter}>
