7c5fcc41750874675278adbf6e1ca19849084b27
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2024-02-20 21:52:26 +0100

    added image

--- src/app/components/Contact/index.tsx
+++ src/app/components/Contact/index.tsx
@@ -1,5 +1,18 @@
+import Image from 'next/image';
 import styles from './Contact.module.scss';
 
 export default function Contact() {
-    return <div className={styles.containerWrap}>CONTACT PAGE</div>;
+    return (
+        <div className={styles.containerWrap}>
+            <div className={styles.overlayWrap}>
+                <div className={styles.imageContainer}>
+                    <Image src="/woman-phone.png" alt="Woman on the phone" width={400} height={395} />
+                </div>
+                <div className={styles.contactContainer}>zostaje na outside nawet kiedy pada deeeszcz</div>
+            </div>
+        </div>
+    );
+}
+{
+    /* <Image src="/logo3.png" alt="Small logo" width={40} height={40} />: */
 }
