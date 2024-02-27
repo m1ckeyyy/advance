17429946898556cd6f686c5c9d08e4fe366a6a06
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2024-02-27 20:16:25 +0100

    offers page

--- src/app/components/LocationOffers/components/CardOffer.tsx
+++ src/app/components/LocationOffers/components/CardOffer.tsx
@@ -1,31 +1,18 @@
 import styles from './CardOffer.module.scss';
 import Image from 'next/image';
 import { DM_Sans } from 'next/font/google';
+import Link from 'next/link';
 
 const dmSans = DM_Sans({ subsets: ['latin'], weight: '300' });
 
-const CardOffer = ({
-    imgsrc,
-    name,
-    classReference,
-}: {
-    imgsrc: string;
-    name: string;
-    classReference: string;
-}) => {
+const CardOffer = ({ imgsrc, name, classReference }: { imgsrc: string; name: string; classReference: string }) => {
     return (
-        <div
-            className={`${styles.cardOffer} ${dmSans.className} ${styles[classReference]} `}
-        >
-            <Image
-                src={imgsrc}
-                width={512 * 1.15}
-                height={398 * 1.21}
-                alt={name}
-                className={styles.image}
-            />
-            <span>{name}</span>
-        </div>
+        <Link href="/offers">
+            <div className={`${styles.cardOffer} ${dmSans.className} ${styles[classReference]} `}>
+                <Image src={imgsrc} width={512 * 1.15} height={398 * 1.21} alt={name} className={styles.image} />
+                <span>{name}</span>
+            </div>
+        </Link>
     );
 };
 
---
+++ src/app/offers/components/OffersSection/index.tsx
@@ -0,0 +1,4 @@
+import styles from './OffersSection.module.scss';
+export default function OffersSection() {
+    return <div className={styles.containerWrap}>AAA</div>;
+}
