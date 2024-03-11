9ec89093a0838194749493fc0ce76ff9a719d4a7
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2024-03-11 10:59:00 +0100

    axios adjusting

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,23 +1,39 @@
 'use client';
 
+import { useState } from 'react';
 import styles from './admin.module.scss';
+import axios from 'axios';
 
 export default function Admin() {
-    //axios
-    const handleSubmit = async (data) => {
-        console.log(data);
-        try {
-            const response = await axios.post('http://127.0.0.1:8080/login', data, {
-                withCredentials: true,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+    const [isVerified, setIsVerified] = useState<boolean>(false);
+    const handleSubmit = async (e: Event) => {
+        e.preventDefault();
+        console.log(e);
+        axios
+            .get('https://worldtimeapi.org/api/ip')
+            .then((res) => {
+                console.log('xd: ', res);
+                if (res.status === 200) {
+                    //to be adjusted
+                }
+                setIsVerified(true);
+            })
+            .catch((err: string) => {
+                console.log('Error: ', err);
             });
-        } catch (error) {
-            console.error(error);
-        }
+
+        // try {
+        //     const response = await axios.post('http://127.0.0.1:8080/login', data, {
+        //         withCredentials: true,
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //         },
+        //     });
+        // } catch (error) {
+        //     console.error(error);
+        // }
     };
-    const callAPI = async (e: Event) => {
+    const callAPI = async (e: any) => {
         e.preventDefault();
         try {
             const res = await fetch(`https://jsonplaceholder.typicode.com/posts/1`);
@@ -27,13 +43,18 @@ export default function Admin() {
             console.log(err);
         }
     };
-
+    const toggleVerify = () => {
+        setIsVerified((prev) => !prev);
+    };
     return (
         <div className={styles.containerWrap}>
             <div className={styles.inputBox}>
-                <form onSubmit={callAPI}>
-                    <button type="submit"></button>
+                <form onSubmit={handleSubmit}>
+                    <h2>hey</h2>
+                    <button type="submit">click me</button>
                 </form>
+                <div className={isVerified ? styles.verified : ''}>{isVerified ? 'User verified' : 'User not verified'}</div>
+                <button onClick={toggleVerify}> toggle verify</button>
             </div>
         </div>
     );
