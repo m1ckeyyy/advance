3c977e04ea19db60e37992825cd0de3c3e6a6fac
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2024-04-26 20:24:57 +0200

    auth

--- src/app/edit-offers/page.tsx
+++ src/app/edit-offers/page.tsx
@@ -2,7 +2,7 @@
 
 import isAuth from '../Utils/isAuth';
 import styles from './EditOffers.module.scss';
-
+// auth middleware
 function EditOffers() {
     return <div className={styles.containerWrap}>hey</div>;
 }
