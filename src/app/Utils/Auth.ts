385eb686e314b97e8d959a2e1256e40a2f10409c
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2024-05-08 14:30:51 +0200

    finished auth

--- src/app/Utils/Auth.ts
+++ src/app/Utils/Auth.ts
@@ -1,14 +1,7 @@
-'use client';
 import axios from 'axios';
-import Cookies from 'js-cookie';
-import { useState } from 'react';
 
-export async function authenticate() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const accessToken = Cookies.get('access_token');
-
-    //console.log('accesstoken siema; ', accessToken);
-
+export async function authenticate(accessToken: string): Promise<boolean> {
+    console.log('run');
     try {
         const response = await axios.post(
             'http://127.0.0.1:4000/auth',
@@ -20,11 +13,9 @@ export async function authenticate() {
                 },
             }
         );
-        setIsAuthenticated(true);
-
-        if (response.status === 200) {
-            // setIsAuthenticated(true);
-        }
-        return true;
-    } catch (e) {}
+        return response.status === 200;
+    } catch (error) {
+        console.error('Authentication error:', error);
+        return false;
+    }
 }
