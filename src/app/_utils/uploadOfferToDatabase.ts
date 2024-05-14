55471fe0ec72f897f0963f4e57e09805dde4b859
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2024-05-14 23:24:50 +0200

    added api and changed folder structure

--- src/app/_utils/uploadOfferToDatabase.ts
+++ src/app/_utils/uploadOfferToDatabase.ts
@@ -1,12 +1,10 @@
 import axios from 'axios';
-import { Offer } from '../offers-dashboard/components/AddOfferForm';
+import { Offer } from '@/app/(routes)/offers-dashboard/components/AddOfferForm';
 
 export async function uploadOfferToDatabase(offer: Partial<Offer>): Promise<boolean> {
     try {
         const response = await axios.post(
-            // 'http://localhost:4000/upload-offer',
-            'http://127.0.0.1:4000/upload-offer',
-            // 'http://3.76.207.157:4000/upload-offer',
+            '/api/upload-offer',
             { offer },
             {
                 withCredentials: true,
---
+++ src/app/api/admin-login/route.ts
@@ -0,0 +1,25 @@
+//admin-login
+import { NextRequest, NextResponse } from 'next/server';
+import mongoose from 'mongoose';
+
+interface LoginRequestBody {
+    email: string;
+    password: string;
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        const requestBody: LoginRequestBody = await request.json();
+        let { email, password } = requestBody;
+
+        if (typeof email !== 'string' || typeof password !== 'string') {
+            throw new Error('Invalid data types for credentials');
+        }
+        email = email.toLowerCase();
+        // Logika uwierzytelniania
+        console.log('This come from api: ', email, password);
+        return NextResponse.json({ message: 'Logged in successfully' }, { status: 200 });
+    } catch (e) {
+        if (e) return NextResponse.json({ message: 'Invalid JSON' }, { status: 400 });
+    }
+}
