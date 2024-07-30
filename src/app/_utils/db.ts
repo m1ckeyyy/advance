bd1f0bedba358cde8003fd160755f133b9ceb4ad
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2024-07-30 20:19:18 +0200

    db integration changes

--- src/app/_utils/db.ts
+++ src/app/_utils/db.ts
@@ -1,18 +1,42 @@
-/*import mongoose from 'mongoose';
-
-const uri = process.env.MONGO_URI;
-
-if (!uri) throw new Error('Database environmental varaible is not defined');
-mongoose.set('strictQuery', false);
-mongoose
-    .connect(uri)
-    .then(() => console.log('Successfully connected to Mongo Database'))
-    .catch((err) => console.log('Error occured while connecting to Mongo Database: ', err));
-*/
 import { MongoClient } from 'mongodb';
+import { NextRequest, NextResponse } from 'next/server';
 
-const uri = process.env.MONGO_URI;
+let cachedDb: any = null;
 
-if (!uri) throw new Error('Database environmental varaible is not defined');
+export async function connectToDatabase() {
+    try {
+        console.log('Connecting to DB...');
+        const uri = process.env.MONGO_URI;
+        console.log('uri', uri);
+        if (!uri) throw new Error('Add MongoDB URI to .env');
 
-export async function connectToDatabase() {}
+        const client = new MongoClient(uri);
+
+        console.log('siema: ', cachedDb);
+    } catch (e: any) {
+        console.log(e.message);
+    }
+    // if (client.isConnected()) {
+    // }
+    // if (req.method === 'GET') {
+    //     const client = new MongoClient(uri);
+    //     try {
+    //         await client.connect();
+
+    //         const database = client.db('mikolaj-cluster');
+    //         const collection = database.collection('advance');
+    //         const allData = await collection.find({}).toArray();
+
+    //         NextResponse.json(allData, { status: 200 });
+    //         console.log('Connected to MongoDB!');
+    //     } catch (error) {
+    //         NextResponse.json({ message: 'Something went wrong!' }, { status: 500 });
+    //         console.log('Error connecting to MongoDB');
+    //     } finally {
+    //         await client.close();
+    //         console.log('MongoDB client closed');
+    //     }
+    // } else {
+    //     NextResponse.json({ message: 'Method not allowed!' }, { status: 405 });
+    // }
+}
--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,3 +1,10 @@
+import { connectToDatabase } from '@/app/_utils/db';
+
 export async function GET(request: Request) {
+    const db = await connectToDatabase();
+    // const users = await db.collection('users').find({}).toArray();
+
+    // res.json(users);
+
     return new Response('helloes');
 }
