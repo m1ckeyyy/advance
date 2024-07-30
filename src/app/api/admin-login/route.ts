f16649ee8a60a892dfbd370f9dd6e53c24b44c32
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2024-07-30 20:19:18 +0200

    db integration changes

--- src/app/api/admin-login/route.ts
+++ src/app/api/admin-login/route.ts
@@ -1,27 +1,24 @@
 //admin-login
 import { NextRequest, NextResponse } from 'next/server';
-import mongoose, { Mongoose } from 'mongoose';
-
 interface LoginRequestBody {
     email: string;
     password: string;
 }
 
 export async function POST(request: NextRequest) {
-    try {
-        const requestBody: LoginRequestBody = await request.json();
-        let { email, password } = requestBody;
-
-        if (typeof email !== 'string' || typeof password !== 'string') {
-            throw new Error('Invalid data types for credentials');
-        }
-        email = email.toLowerCase();
-        // Logika uwierzytelniania
-        mongoose.set();
-
-        console.log('This come from api: ', email, password);
-        return NextResponse.json({ message: 'Logged in successfully' }, { status: 200 });
-    } catch (e) {
-        if (e) return NextResponse.json({ message: 'Invalid JSON' }, { status: 400 });
-    }
+    // try {
+    //     const requestBody: LoginRequestBody = await request.json();
+    //     let { email, password } = requestBody;
+    //     if (typeof email !== 'string' || typeof password !== 'string') {
+    //         throw new Error('Invalid data types for credentials');
+    //     }
+    //     email = email.toLowerCase();
+    //     // Logika uwierzytelniania
+    //     // mongoose.set();
+    //     console.log('This come from api: ', email, password);
+    //     return NextResponse.json({ message: 'Logged in successfully' }, { status: 200 });
+    // } catch (e) {
+    //     console.log(e);
+    //     if (e) return NextResponse.json({ message: 'Invalid JSON, Add email and password in request body' }, { status: 400 });
+    // }
 }
